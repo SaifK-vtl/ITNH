e55ef116bba1ea079af717913a1511ce8aab55cb
SaifK-vtl <saifvitaldesign@gmail.com>
2021-06-09 22:27:38 +0530

    Navigation Menu with category

--- app/code/Mandy/CustomMenu/Block/CustomMenu.php
+++ app/code/Mandy/CustomMenu/Block/CustomMenu.php
@@ -1,62 +1,120 @@
 <?php
+
 namespace Mandy\CustomMenu\Block;
 
 class CustomMenu extends \Magento\Framework\View\Element\Template
 {
-    protected $_categoryHelper;
-    protected $categoryFlatConfig;
-    protected $topMenu;
     /**
-     * @param \Magento\Framework\View\Element\Template\Context $context
+     * @var \Magento\Catalog\Helper\Category
+     */
+    protected $_categoryHelper;
+
+    /**
+     * @var \Magento\Catalog\Model\Indexer\Category\Flat\State
+     */
+    protected $categoryFlatConfig;
+
+    /**
+     * @var \Magento\Catalog\Model\CategoryFactory
+     */
+    protected $_categoryFactory;
+
+    /**
+     * @param Template\Context $context
      * @param \Magento\Catalog\Helper\Category $categoryHelper
+     * @param \Magento\Catalog\Model\Indexer\Category\Flat\State $categoryFlatState
+     * @param \Magento\Catalog\Model\CategoryFactory $categoryFactory
      * @param array $data
      */
     public function __construct(
         \Magento\Framework\View\Element\Template\Context $context,
         \Magento\Catalog\Helper\Category $categoryHelper,
         \Magento\Catalog\Model\Indexer\Category\Flat\State $categoryFlatState,
-        \Magento\Theme\Block\Html\Topmenu $topMenu
-    ) {
+        \Magento\Catalog\Model\CategoryFactory $categoryFactory,
+        $data = []
+    )
+    {
         $this->_categoryHelper = $categoryHelper;
         $this->categoryFlatConfig = $categoryFlatState;
-        $this->topMenu = $topMenu;
-        parent::__construct($context);
-    }
-    /**
-     * Return categories helper
-     */
-    public function getCategoryHelper()
-    {
-        return $this->_categoryHelper;
-    }
-    /**
-     * Return top menu html
-     * getHtml($outermostClass = '', $childrenWrapClass = '', $limit = 0)
-     * example getHtml('level-top', 'submenu', 0)
-     */
-    public function getHtml()
-    {
-        return $this->topMenu->getHtml();
-    }
-    /**
-     * Retrieve current store categories
-     *
-     * @param bool|string $sorted
-     * @param bool $asCollection
-     * @param bool $toLoad
-     * @return \Magento\Framework\Data\Tree\Node\Collection|\Magento\Catalog\Model\Resource\Category\Collection|array
-     */
-    public function getStoreCategories($sorted = false, $asCollection = false, $toLoad = true)
-    {
-        return $this->_categoryHelper->getStoreCategories($sorted, $asCollection, $toLoad);
+        $this->_categoryFactory = $categoryFactory;
+        parent::__construct($context, $data);
     }
 
     /**
-     * Retrieve child store categories
+     * Get all categories
+     *
+     * @param bool $sorted
+     * @param bool $asCollection
+     * @param bool $toLoad
+     *
+     * @return array|\Magento\Catalog\Model\ResourceModel\Category\Collection|\Magento\Framework\Data\Tree\Node\Collection
+     */
+    public function getCategories($sorted = false, $asCollection = false, $toLoad = true)
+    {
+        $cacheKey = sprintf('%d-%d-%d-%d', $this->getSelectedRootCategory(), $sorted, $asCollection, $toLoad);
+        if (isset($this->_storeCategories[$cacheKey])) {
+            return $this->_storeCategories[$cacheKey];
+        }
+        /**
+         * Check if parent node of the store still exists
+         */
+        $category = $this->_categoryFactory->create();
+
+        $storeCategories = $category->getCategories($this->getSelectedRootCategory(), $recursionLevel = 0, $sorted, $asCollection, $toLoad);
+        $this->_storeCategories[$cacheKey] = $storeCategories;
+        return $storeCategories;
+    }
+
+    /**
+     * Get current store root category id
+     *
+     * @return int|mixed
+     */
+    public function getSelectedRootCategory()
+    {
+        return $this->_storeManager->getStore()->getRootCategoryId();
+    }
+
+    /**
+     * @param        $category
+     * @param string $html
+     * @param int $level
+     *
+     * @return string
+     */
+    public function getChildCategoryView($category, $html = '', $level = 1)
+    {
+        // Check if category has children
+        if ($category->hasChildren()) {
+            $childCategories = $this->getSubcategories($category);
+            $childCount = (int)$category->getChildrenCount();
+            if ($childCount > 0) {
+                $html .= '<ul class="menus o-list o-list--unstyled">';
+                // Loop through children categories
+                foreach ($childCategories as $childCategory) {
+                    $html .= '<li class="level' . $level . ' has-submenu"> ';
+                    $html .= '<a href="' . $this->getCategoryUrl($childCategory) . '" title="' . $childCategory->getName() . '">' . $childCategory->getName() . '</a>';
+                    if ($childCategory->hasChildren()) {
+                        $html .= '<ul class="submenu">';
+                        $html .= $this->getChildCategoryView($childCategory, '', ($level + 1));
+                        $html .= '</ul>';
+                    }
+                    $html .= '</li>';
+                }
+                $html .= '</ul>';
+            }
+        }
+        return $html;
+    }
+
+    /**
+     * Retrieve subcategories
+     *
      * @param $category
+     *
      * @return array
      */
-    public function getChildCategories($category)
+    public function getSubcategories($category)
     {
         if ($this->categoryFlatConfig->isFlatEnabled() && $category->getUseFlatResource()) {
             $subcategories = (array)$category->getChildrenNodes();
@@ -64,5 +122,17 @@ class CustomMenu extends \Magento\Framework\View\Element\Template
             $subcategories = $category->getChildren();
         }
         return $subcategories;
+    }
+
+    /**
+     * Return Category URL
+     *
+     * @param $category
+     *
+     * @return string
+     */
+    public function getCategoryUrl($category)
+    {
+        return $this->_categoryHelper->getCategoryUrl($category);
     }
 }
